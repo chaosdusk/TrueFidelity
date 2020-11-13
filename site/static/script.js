003988e52718b033c250bc8b746d44707b5184ef
arthurliu <theonlyarthurliu@gmail.com>
2020-11-12 23:34:57 -0800

    drawing lines on a canvas

---
+++ site/static/script.js
@@ -0,0 +1,92 @@
+var canvas = document.getElementById("paint");
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
+var curX, curY, prevX, prevY;
+var hold = false;
+ctx.lineWidth = 2;
+var fill_value = true;
+var stroke_value = false;
+var canvas_data = {"pencil": [], "line": [], "rectangle": [], "circle": [], "eraser": []}
+
+// line tool
+        
+function line(){
+           
+    canvas.onmousedown = function (e){
+        img = ctx.getImageData(0, 0, width, height);
+        prevX = e.clientX - canvas.offsetLeft;
+        prevY = e.clientY - canvas.offsetTop;
+        hold = true;
+    };
+            
+    canvas.onmousemove = function linemove(e){
+        if (hold){
+            // ctx.putImageData(img, 0, 0);
+            curX = e.clientX - canvas.offsetLeft;
+            curY = e.clientY - canvas.offsetTop;
+            ctx.beginPath();
+            ctx.moveTo(prevX, prevY);
+            ctx.lineTo(curX, curY);
+            // ctx.stroke();
+            // canvas_data.line.push({ "starx": prevX, "starty": prevY, "endx": curX, "endY": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
+            ctx.closePath();
+        }
+    };
+            
+    canvas.onmouseup = function (e){
+         hold = false;
+    };
+            
+    canvas.onmouseout = function (e){
+         hold = false;
+    };
+}
+ 
+// eraser tool
+        
+function eraser(){
+    
+    canvas.onmousedown = function(e){
+        curX = e.clientX - canvas.offsetLeft;
+        curY = e.clientY - canvas.offsetTop;
+        hold = true;
+            
+        prevX = curX;
+        prevY = curY;
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+    };
+        
+    canvas.onmousemove = function(e){
+        if(hold){
+            curX = e.clientX - canvas.offsetLeft;
+            curY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    };
+        
+    canvas.onmouseup = function(e){
+        hold = false;
+    };
+        
+    canvas.onmouseout = function(e){
+        hold = false;
+    };
+        
+    function draw(){
+        ctx.lineTo(curX, curY);
+        ctx.strokeStyle = "#ffffff";
+        ctx.stroke();
+        canvas_data.pencil.push({ "startx": prevX, "starty": prevY, "endx": curX, "endy": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
+    }    
+}  
+
+function save(){
+    var filename = document.getElementById("fname").value;
+    var data = JSON.stringify(canvas_data);
+    var image = canvas.toDataURL();
+    
+    $.post("/", { save_fname: filename, save_cdata: data, save_image: image });
+    alert(filename + " saved");
+} 
