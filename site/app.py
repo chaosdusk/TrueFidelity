ccb005d9367528966593c411e052b1ffe7d9f1b6
arthurliu <theonlyarthurliu@gmail.com>
2020-11-12 22:57:56 -0800

    Serving images

--- site/app.py
+++ site/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask import Flask, render_template, url_for, request, redirect, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -8,6 +8,35 @@ def index():
     # return render_template('index.html', tasks=tasks)
     return render_template('index.html')
 
+@app.route("/simple.png")
+def simple():
+    import datetime
+    from io import BytesIO
+    import random
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
