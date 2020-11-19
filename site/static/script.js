733f1620fcf0e8f2c3862fa2ae7849f115ea538c
arthurliu <theonlyarthurliu@gmail.com>
2020-11-19 00:32:37 -0800

    multiple canvases working

--- site/static/script.js
+++ site/static/script.js
@@ -1,45 +1,52 @@
-var canvas = document.getElementById("paint");
-var ctx = canvas.getContext("2d");
-var width = canvas.width;
-var height = canvas.height;
-var curX, curY, prevX, prevY;
-var hold = false;
-ctx.lineWidth = 2;
+var canvases = document.getElementsByTagName('canvas');
 
-var input = document.getElementById("fname")
+for (var i = 0; i < canvases.length; i++) {
+    setupCanvas(canvases[i])
+}
 
-canvas.onmousedown = function (e){
-    var BB = canvas.getBoundingClientRect();
+function setupCanvas(canvas) {
+    var ctx = canvas.getContext("2d");
+    var width = canvas.width;
+    var height = canvas.height;
+    var curX, curY, prevX, prevY;
+    var hold = false;
+    ctx.lineWidth = 2;
 
-    img = ctx.getImageData(0, 0, width, height);
-    prevX = e.clientX - BB.left;
-    prevY = e.clientY - BB.top
-    hold = true;
-};
+    var input = document.getElementById("length")
 
-canvas.onmousemove = function linemove(e){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (hold){
+    canvas.onmousedown = function (e){
         var BB = canvas.getBoundingClientRect();
-        console.log("hi");
-        ctx.putImageData(img, 0, 0);
-        curX = e.clientX - BB.left;
-        curY = e.clientY - BB.top;
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(curX, curY);
-        ctx.stroke();
-        // canvas_data.line.push({ "starx": prevX, "starty": prevY, "endx": curX, "endY": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
-        ctx.closePath();
-        input.value = Math.sqrt((curX - prevX) ** 2 + (curY - prevY) ** 2).toFixed(2);
-    }
-};
 
-canvas.onmouseup = function (e){
-        hold = false;
-};
+        img = ctx.getImageData(0, 0, width, height);
+        prevX = e.clientX - BB.left;
+        prevY = e.clientY - BB.top
+        hold = true;
+    };
 
-canvas.onmouseout = function (e){
-        hold = false;
-};
+    canvas.onmousemove = function linemove(e){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (hold){
+            var BB = canvas.getBoundingClientRect();
+            ctx.putImageData(img, 0, 0);
+            curX = e.clientX - BB.left;
+            curY = e.clientY - BB.top;
+            ctx.beginPath();
+            ctx.moveTo(prevX, prevY);
+            ctx.lineTo(curX, curY);
+            ctx.stroke();
+            // canvas_data.line.push({ "starx": prevX, "starty": prevY, "endx": curX, "endY": curY, "thick": ctx.lineWidth, "color": ctx.strokeStyle });
+            ctx.closePath();
+            input.value = Math.sqrt((curX - prevX) ** 2 + (curY - prevY) ** 2).toFixed(2);
+        }
+    };
+
+    canvas.onmouseup = function (e){
+            hold = false;
+    };
+
+    canvas.onmouseout = function (e){
+            hold = false;
+    };
+}
+
 
