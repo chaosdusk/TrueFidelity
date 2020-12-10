28b62e96daac6b38cf6fc960794f9bc6b13088cd
Edward Tan <edward.z.tan.1999@gmail.com>
2020-12-09 22:11:06 -0800

    hu scaling

--- dicom_processor.py
+++ dicom_processor.py
@@ -28,14 +28,17 @@ def noise_mod(image, index):
         offset_y = -50
     return (noise[0]+offset_y, noise[1]+offset_x)
 
+def transform_to_hu(original, pixels):
+    return original.RescaleIntercept + pixels * original.RescaleSlope
+
 def generate(image_index, image_full_path, lesion_index, is_noise=False):
     img = pydicom.read_file(image_full_path)
 
     # process image
     pixels = img.pixel_array.astype(float)
-    scaled = np.uint8((np.maximum(pixels, 0) / pixels.max()) * 255.0)
+    #scaled = np.uint8((np.maximum(pixels, 0) / pixels.max()) * 255.0)
     noise = np.full((radius*2*grid_dim, radius*2*grid_dim), 1024)
-
+    scaled = noise = transform_to_hu(img, pixels)
     # 4x4 grid of noise
     for i in range(0, radius*2*(grid_dim-1)+1, radius*2):
         for j in range(0, radius*2*(grid_dim-1)+1, radius*2):
@@ -52,7 +55,7 @@ def generate(image_index, image_full_path, lesion_index, is_noise=False):
         # select random grid to replace with the lesion cut
         lesion_x = random.randint(0, grid_dim - 1)
         lesion_y = random.randint(0, grid_dim - 1)
-        lesion_selected = location_mod(image_index, 0)
+        lesion_selected = location_mod(image_index, lesion_index)
         lesion = scaled[lesion_selected[0]-radius:lesion_selected[0]+radius, lesion_selected[1]-radius:lesion_selected[1]+radius]
         noise[lesion_y*radius*2:lesion_y*radius*2+radius*2, lesion_x*radius*2:lesion_x*radius*2+radius*2] = lesion
         #plt.imshow(noise, cmap='gray', vmin=0, vmax=255)
@@ -65,19 +68,20 @@ def generate(image_index, image_full_path, lesion_index, is_noise=False):
         else:
             hu = -5
     size = lesion_sizes[lesion_index % 5]
-
+    image_folder = '../TFImageSet/images_{}x{}'.format(grid_dim, grid_dim)
+    noise_folder = '../TFImageSet/noise_{}x{}'.format(grid_dim, grid_dim)
     image_number = 0
-    save_file = './images_5x5/{};{};{};{};{}.pickle'.format(directories[image_index][8:], hu, 'lesion', size, image_number) if not is_noise else './noise_5x5/{};{}.pickle'.format(directories[image_index][8:], 'noise')
-
-    if not os.path.exists('noise_5x5'):
-        os.makedirs('noise_5x5')
+    save_file = '{}/{};{};{};{};{}.pickle'.format(image_folder, directories[image_index][8:], hu, 'lesion', size, image_number) if not is_noise else '{}/{};{}.pickle'.format(noise_folder, directories[image_index][8:], 'noise')
+    save_path = image_folder if not is_noise else noise_folder
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     with open(save_file, 'wb') as handle:
         pickle.dump(noise, handle)
 
 
 # setup
 radius = 15
-grid_dim = 5
+grid_dim = 6
 lesion_locations = [(250, 390), (230, 355), (205, 305), (190, 270), (175, 245), 
                     (300, 365), (280, 330), (255, 280), (240, 245), (225, 220),
                     (350, 340), (330, 305), (305, 255), (290, 220), (275, 195)]
@@ -87,7 +91,7 @@ offset = 20
 # get random image to pull lesion and noise from
 directories = [x[0] for x in os.walk('./DICOM/')]
 for image_index in range(1, len(directories)):
-    for lesion_index in range(0, 1):
+    for lesion_index in range(0, 15):
         image_path = directories[image_index]
         (_, _, image_file) = next(os.walk(image_path))
         image_full_path = '{}/{}'.format(image_path, image_file[0])
